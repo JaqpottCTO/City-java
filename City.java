13fb04d86abad44913246326184878e6a1d9f919
Steven C Philpott Sr <steven.philpott@evertisky.com>
2024-07-04 14:55:45 -0500

    Create City.java

---
+++ City.java
@@ -0,0 +1,68 @@
+package com.example.uamcityattributeanalyzer.model;
+
+public class City {
+    private String name;
+    private double latitude;
+    private double longitude;
+    private int population;
+    private double commercialActivityIndex;
+    private double tourismIndex;
+
+    public City(String name, double latitude, double longitude, int population, double commercialActivityIndex, double tourismIndex) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.population = population;
+        this.commercialActivityIndex = commercialActivityIndex;
+        this.tourismIndex = tourismIndex;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public double getCommercialActivityIndex() {
+        return commercialActivityIndex;
+    }
+
+    public void setCommercialActivityIndex(double commercialActivityIndex) {
+        this.commercialActivityIndex = commercialActivityIndex;
+    }
+
+    public double getTourismIndex() {
+        return tourismIndex;
+    }
+
+    public void setTourismIndex(double tourismIndex) {
+        this.tourismIndex = tourismIndex;
+    }
+}
